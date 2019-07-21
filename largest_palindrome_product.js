52627a125b713bcd7282d8424a47921976bb4fda
Briii <brianwkhuffman@gmail.com>
2019-07-20 15:07:10 -1000

    Touched up, one universal function

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -9,58 +9,33 @@ module.exports = function(digits) {
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
-  let f1Arr = [];
-  let f2Arr = [];
   let palArr = [];
   // do your work here
-  if (digits === 2) {
-    for (let i = 99; i >= 10; i--) {
-      factor_0 = i;
-      for (let j = 99; j >= 10; j--) {
-        factor_1 = j;
-        let result = factor_0 * factor_1 + "";
-        if (
-          result ===
-          result
-            .split("")
-            .reverse()
-            .join("")
-        ) {
-          f1Arr.push(i);
-          f2Arr.push(j);
-          palArr.push(result);
-        }
-      }
-    }
-    factor_0 = f1Arr[0];
-    factor_1 = f2Arr[0];
-    let palArrNum = palArr.map(Number);
-    palindromeNumber = palArrNum[0];
-  }
+  let f1Arr = [];
+  let f2Arr = [];
+  let num = Math.pow(10, digits) - 1;
+  let num2 = Math.pow(10, digits - 1);
 
-  if (digits === 3) {
-    for (let i = 999; i >= 100; i--) {
-      factor_0 = i;
-      for (let j = 999; j >= 100; j--) {
-        factor_1 = j;
-        let result = factor_0 * factor_1 + "";
-        if (
-          result ===
-          result
-            .split("")
-            .reverse()
-            .join("")
-        ) {
-          f1Arr.push(i);
-          f2Arr.push(j);
-          palArr.push(result);
-        }
+  for (let i = num; i > num2; i--) {
+    for (let j = num; j > num2; j--) {
+      let result = i * j + "";
+      if (
+        result ===
+        result
+          .split("")
+          .reverse()
+          .join("")
+      ) {
+        palArr.push(result);
+        f1Arr.push(i);
+        f2Arr.push(j);
       }
     }
-    let palArrNum = palArr.map(Number);
-    let maxPalArr = Math.max(...palArrNum); //gives max number in the array since, for ex, 999*583 will be evaluated before 999*997
-    palindromeNumber = maxPalArr;
   }
+  let highNum = Math.max(...palArr);
+  factor_0 = f1Arr[0];
+  factor_1 = f2Arr[0];
+  palindromeNumber = highNum;
 
   return {
     factor_0: factor_0,
